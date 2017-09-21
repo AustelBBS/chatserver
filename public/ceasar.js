7d52a83106b3abe738b0241d69e5c3b3b022affa
Omar <omarweeb@gmail.com>
2017-09-21 01:37:04 -0500

    Nueva version de la función de encriptar. Bugs deberían estar corregidos.

--- public/ceasar.js
+++ public/ceasar.js
@@ -1,3 +1,30 @@
+function encrypt (inputString, shiftedpositions, direction){
+  if(direction === '>'){
+    //encriptar normal, nada que hacer
+    shiftedpositions = shiftedpositions
+  } else if (direction === '<') {
+    //desencriptar, lo mismo pero con - shiftedpositions
+    shiftedpositions = (-1) * shiftedpositions
+  }
+  else {
+    return "Error, direction can be either > or <.";
+  }
+  var salida = "";
+  var oldASCII; //donde se guarda el codigo ascii de una letra
+  var newASCII;//codigo ascii resultante luego de sumarle shiftedpositions
+  //por cada letra de la entrada
+  for(var c = 0; c < inputString.length; c++){
+    oldASCII = inputString[c].charCodeAt();//obtenemos su codigo
+    newASCII = oldASCII + shiftedpositions;//desplazamos de lugar la letra al sumarle shiftedpositions
+    salida = salida.concat(String.fromCharCode(newASCII));//convertimos el nuevo codigo a string y concatenamos
+  }
+  return salida;
+}
+
+
+
+
+/*Depreciated:- Contains bugs
 function encrypt(inputString, shiftedpositions, direction) {
   //Abecedario
   var ABC = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','ñ','o','p','q','r','s','t','u','v','w','x','y','z'];
@@ -64,4 +91,4 @@ function encrypt(inputString, shiftedpositions, direction) {
     //excepciones aqui
     return "Parametro incorrecto";
   }
-}
+}*/
