8d589b22fa864dee4af69a024878fc8d18429a51
AustelBBS <orvehi@gmail.com>
2017-09-22 00:07:24 -0500

    Actualizacion de seguridad

--- index.js
+++ index.js
@@ -5,34 +5,34 @@ var io = require('socket.io')(http);
 var seed;
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.use(exp.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-	console.log('Un usuario se ha conectado');
-	socket.on('disconnect', function(){
-		console.log('Un usuario se ha desconectado');
-	});
+  console.log('Un usuario se ha conectado');
+  socket.on('disconnect', function(){
+    console.log('Un usuario se ha desconectado');
+  });
   
   socket.on('seed', function(s){
     seed = s;
     console.log('seed: ' +  seed);
   });
-	
+  
   socket.on('chat message', function(key){
-		var msg = encrypt(key, -seed);
-		console.log('key: ' + key);
-		console.log('msg: ' + msg);
-		io.emit('chat message', msg);
-	});
+    var msg = encrypt(key, -seed);
+    console.log('key: ' + key);
+    console.log('msg: ' + msg);
+    io.emit('chat message', msg);
+  });
 
 });
 
 
 http.listen(3000, function(){
-	console.log('Escuchando en *:3000');
+  console.log('Escuchando en *:3000');
 });
 
 function encrypt (inputString, shiftedpositions){
