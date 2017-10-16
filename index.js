25e8cdb438a7a7b8650713dac43ca6536155d4b2
AustelBBS <orvehi@gmail.com>
2017-10-15 22:43:03 -0500

    Agregado los usuarios y los mensajes para actualizar listas de usuarios

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ var exp = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var seed;
+//var nsp = io.of('/name');
+var users = [];
+var ids = [];
+
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -11,19 +14,22 @@ app.get('/', function(req, res){
 app.use(exp.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
-  console.log('Un usuario se ha conectado');
-  socket.on('disconnect', function(){
-    console.log('Un usuario se ha desconectado');
+  //agregar usuarios
+  socket.on('adduser', function(name, id){
+    users.push(name);
+    //remueve los usuarios
+    console.log(name +' se ha conectado');
+    socket.once('disconnect', function(){
+      var pos = users.indexOf(name);
+      if(pos >= 0 && ids[pos] == id)
+        users.splice(pos, 1);
+        ids.splice(pos, 1);
+        console.log(name +' se ha desconectado');
+    });
+    socket.emit('updateUsers', users, ids);
   });
   
-  socket.on('seed', function(s){
-    seed = s;
-    console.log('seed: ' +  seed);
-  });
-  
-  socket.on('chat message', function(key){
-    var msg = encrypt(key, -seed);
-    console.log('key: ' + key);
+  socket.on('chat message', function(msg){
     console.log('msg: ' + msg);
     io.emit('chat message', msg);
   });
@@ -34,16 +40,3 @@ io.on('connection', function(socket){
 http.listen(3000, function(){
   console.log('Escuchando en *:3000');
 });
-
-function encrypt (inputString, shiftedpositions){
-  var salida = "";
-  var oldASCII; //donde se guarda el codigo ascii de una letra
-  var newASCII;//codigo ascii resultante luego de sumarle shiftedpositions
-  //por cada letra de la entrada
-  for(var c = 0; c < inputString.length; c++){
-    oldASCII = inputString[c].charCodeAt();//obtenemos su codigo
-    newASCII = oldASCII + shiftedpositions;//desplazamos de lugar la letra al sumarle shiftedpositions
-    salida = salida.concat(String.fromCharCode(newASCII));//convertimos el nuevo codigo a string y concatenamos
-  }
-  return salida;
-}
