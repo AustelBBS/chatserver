f569f12fd95699444a22b3edfd204d3a0e1c6f22
AustelBBS <orvehi@gmail.com>
2017-10-17 13:19:05 -0500

    Agregado funciones para mensajeria tipo IRC 1 a 1

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 //var nsp = io.of('/name');
-var users = [];
-var ids = [];
+var users = {};
 
 
 app.get('/', function(req, res){
@@ -15,27 +14,47 @@ app.use(exp.static(__dirname + '/public'));
 
 io.on('connection', function(socket){
   //agregar usuarios
-  socket.on('adduser', function(name, id){
-    users.push(name);
-    //remueve los usuarios
+  socket.on('adduser', function(name){
+    //agrega el nombre de usuario al arreglo
+    socket.nickname = name;
     console.log(name +' se ha conectado');
+    users[socket.nickname] = socket;
+    updateUsers();    
     socket.once('disconnect', function(){
-      var pos = users.indexOf(name);
-      if(pos >= 0 && ids[pos] == id)
-        users.splice(pos, 1);
-        ids.splice(pos, 1);
-        console.log(name +' se ha desconectado');
+      if(!socket.nickname) return;
+      delete users[socket.nickname];
+      updateUsers();
     });
-    socket.emit('updateUsers', users, ids);
   });
-  
-  socket.on('chat message', function(msg){
+
+  socket.on('chat message', function(user, msg){
+    msg = msg.trim();
+    if(msg.substr(0, 3) === '/w ') {
+      msg = msg.substr(3);
+      var ind = msg.indexOf(' ');
+      if(ind !== -1) {
+        var nick = msg.substr(0, ind);
+        msg = msg.substr(ind+1);
+        if(nick in users) {
+          users[nick].emit('whisper', user, msg);
+          console.log('User is being sneaky');
+        } else {
+          console.log('Error, nickname not in array');
+        }
+      } else {
+        console.log('Error');
+      }
+    } else {
+      io.emit('chat message', user, msg);  
+    }
     console.log('msg: ' + msg);
-    io.emit('chat message', msg);
   });
 
 });
 
+function updateUsers() {
+  io.emit('updateUsers', Object.keys(users));
+}
 
 http.listen(3000, function(){
   console.log('Escuchando en *:3000');
